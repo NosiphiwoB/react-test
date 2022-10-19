09f295b8681ac1c91c9972d798d4e961ff58cfe3
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-10-19 10:27:39 +0200

    react test

--- form-test/src/App.js
+++ form-test/src/App.js
@@ -2,44 +2,23 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-  const [name, setName] = useState("");
- const [surname, setSurname] = useState("");
- const [age, setAge] = useState("");
- const [radioButtons, setRadioButtons] = useState();
+ const [form,setform] = useState({firstName:'',surname:'',age:'',trasport:''})
  const [list, setList] = useState([]);
 
 
-const nameHandler = (e) => {
-  setName(e.target.value)
+const handlechange = (e) =>{
+  setform({...form,[e.target.name]:e.target.value})
 }
-
-const surnameHandler = (e) => {
-  setSurname(e.target.value)
-}
-
-const ageHandler = (e) => {
-  setAge(e.target.value)
-}
-
-const radiobuttonHandler = (e) => {
-  setRadioButtons(e.target.value)
-}
-
-// const valuesHandler = (e) => {
-//   setName(e.target.value);
-//   setSurname(e.target.value);
-//   setAge(e.target.value);
-//   setRadioButtons(e.target.value);
-// }
-
 
  const handleSubmit = (e) => {
   e.preventDefault();
-    setList([...list, {firstname: name,lastname: surname,age: age,trans: radioButtons}]);
-    setName("");
-    setSurname("");
-    setAge("");
-    setRadioButtons("");
+    setList([...list, form]);
+    setform({
+      firstName:"",
+      surname:"",
+      age:"",
+      trasport:"",
+    });
  }
 
 console.log(list)
@@ -49,13 +28,13 @@ console.log(list)
     <div className="form">
       <h1>React Form</h1>
        <div>
-     <input placeholder="Name" type="text" name={name} onChange={nameHandler} ></input>
+     <input placeholder="Name" type="text" name={'firstName'} value={form.firstName} onChange={handlechange} ></input>
      </div>
      <div>
-     <input placeholder="Surname" type="text" name={surname} onChange={surnameHandler}></input>
+     <input placeholder="Surname" type="text" name={'surname'} value={form.surname} onChange={handlechange}></input>
      </div>
      <div>
-     <input placeholder="Age" type="number" id="age" name={age} onChange={ageHandler}></input>
+     <input placeholder="Age" type="number" id="age" name={'age'} value={form.age} onChange={handlechange}></input>
      </div>
 
      <div>
@@ -64,12 +43,12 @@ console.log(list)
     </div>
 
     <div>
-      <input type="checkbox" value="yes" name={radioButtons} onChange={radiobuttonHandler} checked={radioButtons ==="yes"}></input>
+      <input type="checkbox" value="yes" name={"trasport"} onChange={handlechange} checked={form.trasport ==="yes"}></input>
       <label>Yes</label>
     </div>
 
     <div>
-     <input type="checkbox" value="no" name={radioButtons} onChange={radiobuttonHandler} checked={radioButtons ==="no"}></input>
+     <input type="checkbox" value="no" name={"trasport"} onChange={handlechange} checked={form.trasport ==="no"}></input>
      <label>No</label>
     </div>
    </div>
